aa69d3506a258d88c14b4180c4dbc2631dfbff94
aokyut <epaperamic@gmail.com>
2019-12-12 14:22:46 +0900

    create sample_api.py and dockerfile

---
+++ sample_api.py
@@ -0,0 +1,14 @@
+from flask import Flask,request
+
+app = Flask(__name__)
+
+@app.route("/",methods=["POST"])
+def post_json():
+    posted_json = request.get_json()
+    src_path = posted_json["src"]
+    ref_path = posted_json["ref"]
+    reso = int(posted_json["reso"])
+
+    return src_path
+
+app.run(port=3001)
